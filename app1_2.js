49186543af21d1873aaedb899901695b8cb084e6
linejie <linjie@isspu.com>
2016-09-20 16:48:25 +0800

    从单页扩展到多页

---
+++ app1_2.js
@@ -0,0 +1,76 @@
+var express = require('express')
+var superagent = require('superagent')  //专注于处理服务端/客户端的http请求
+var cheerio = require('cheerio')    //向jq一样操纵数据
+var eventproxy = require('eventproxy')
+var fs = require('fs')
+
+var app = express(),
+    ep = new eventproxy(),
+    imgUrlsArray = [],  //存放网页中的图片链接
+    pageUrls = [],      //存放需要爬取图片的网页
+    pageNum = 2,        // 要取几页
+    randomPre = 'dl' + parseInt(Math.random()*100000) + '-', //起名字用的
+    counter = 1;        // 起名字的计数器
+    dir = './images'    // 放图片的目录
+
+
+// var pageStart = 1914
+// pageUrls.push('http://jandan.net/pic/page-'+ curPage +'#comments')
+//
+var pageStart = 1
+for (var i = pageStart ; i <= pageNum ; i++) {
+  var curPage = i
+  pageUrls.push('http://dnf.17173.com/2014pic/214zxtp_'+ curPage +'.shtml')
+}
+
+// console.log(pageUrls)
+console.log('start--------')
+
+app.get('/', function(req, res, next) {
+  pageUrls.forEach( function(pageUrls){
+    superagent.get(pageUrls)
+      .end(function(err, sres) {
+        if(err) {
+          return next(err)
+        }
+
+        var $ = cheerio.load(sres.text)
+        var curImgUrls = $('.comm-pn-bd img')
+
+        for(var i = 0 ; i < curImgUrls.length ; i++){
+          var curImgUrl = curImgUrls.eq(i).attr('src')
+          console.log('curImgUrl:', curImgUrl)
+          imgUrlsArray.push(curImgUrl)
+          download(curImgUrl, dir ,randomPre + counter + curImgUrl.substr(-4, 4))
+          counter++;
+          // ep.emit('imgUrlGet', cureImgUrl)
+        }
+      })
+  })
+
+  // ep.after('imgUrlGet', imgUrlsArray.length, function(curImgUrl) {
+  //   // console.log('curImgUrl:', curImgUrl)
+
+  //   if(curImgUrl.length !== 0) {
+  //     download(curImgUrl, randomPre + counter + curImgUrl.substr(-4, 4))
+  //   }
+
+  //   counter++;
+  // })
+
+})
+
+function download(url, dir, filename) {
+  superagent.head(url, function(err, res, body) {
+    if(err) {
+      console.log('err: ', err)
+      return;
+    }
+    superagent(url).pipe(fs.createWriteStream(dir + "/" + filename)) //stream
+  })
+}
+
+
+app.listen(3000, function(req, res) {
+  console.log('app is running at port 3000')
+})
